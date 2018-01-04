4368fbf2f7dac1d2608e4d6aab140d8ef9758a0c
crossoverJie <crossoverjie@gmail.com>
2018-01-04 21:00:00 +0800

    二叉树 中序遍历 单测

---
+++ src/test/java/com/crossoverjie/algorithm/BinaryNodeTest.java
@@ -0,0 +1,19 @@
+package com.crossoverjie.algorithm;
+
+import org.junit.Test;
+
+public class BinaryNodeTest {
+
+    @Test
+    public void test1(){
+        BinaryNode node = new BinaryNode() ;
+        //创建二叉树
+        node = node.createNode() ;
+        System.out.println(node);
+
+        //中序遍历二叉树
+        node.levelIterator(node) ;
+
+    }
+
+}
