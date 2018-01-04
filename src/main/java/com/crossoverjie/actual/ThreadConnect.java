ddb2885b328500d652e1ed1f37c5e2b175a8c2bb
crossoverJie <crossoverjie@gmail.com>
2018-01-04 23:54:27 +0800

    三种线程通信方式

---
+++ src/main/java/com/crossoverjie/actual/ThreadConnect.java
@@ -0,0 +1,130 @@
+package com.crossoverjie.actual;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.concurrent.*;
+
+/**
+ * Function:三种方式线程通信
+ *
+ * @author crossoverJie
+ *         Date: 04/01/2018 22:57
+ * @since JDK 1.8
+ */
+public class ThreadConnect {
+    private final static Logger LOGGER = LoggerFactory.getLogger(ThreadConnect.class);
+    public static void main(String[] args) throws Exception {
+        //join();
+        //executorService();
+        countDownLatch();
+
+    }
+
+    /**
+     * 使用countDownLatch 每执行完一个就减一，最后等待全部完成
+     * @throws Exception
+     */
+    private static void countDownLatch() throws Exception{
+        int thread = 3 ;
+        long start = System.currentTimeMillis();
+        CountDownLatch countDown = new CountDownLatch(thread);
+        for (int i= 0 ;i<thread ; i++){
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    LOGGER.info("thread run");
+                    try {
+                        Thread.sleep(2000);
+                        countDown.countDown();
+
+                        LOGGER.info("thread end");
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }).start();
+        }
+
+        countDown.await();
+        long stop = System.currentTimeMillis();
+        LOGGER.info("main over total time={}",stop-start);
+    }
+
+    /**
+     * 利用线程池的 awaitTermination 方法，每隔一秒钟检查线程池是否执行完毕
+     * @throws Exception
+     */
+    private static void executorService() throws Exception{
+        BlockingQueue<Runnable> queue = new LinkedBlockingQueue<>(10) ;
+        ThreadPoolExecutor poolExecutor = new ThreadPoolExecutor(5,5,1, TimeUnit.MILLISECONDS,queue) ;
+        poolExecutor.execute(new Runnable() {
+            @Override
+            public void run() {
+                LOGGER.info("running");
+                try {
+                    Thread.sleep(3000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        poolExecutor.execute(new Runnable() {
+            @Override
+            public void run() {
+                LOGGER.info("running2");
+                try {
+                    Thread.sleep(2000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        poolExecutor.shutdown();
+        while (!poolExecutor.awaitTermination(1,TimeUnit.SECONDS)){
+            LOGGER.info("线程还在执行。。。");
+        }
+        LOGGER.info("main over");
+    }
+
+
+    /**
+     * 采用 join 线程间通信
+     * @throws InterruptedException
+     */
+    private static void join() throws InterruptedException {
+        Thread t1 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                LOGGER.info("running");
+                try {
+                    Thread.sleep(3000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }) ;
+        Thread t2 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                LOGGER.info("running2");
+                try {
+                    Thread.sleep(4000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }) ;
+
+        t1.start();
+        //等待线程1终止
+        t1.join();
+
+        t2.start();
+        //等待线程2终止
+        t2.join();
+
+        LOGGER.info("main over");
+    }
+}
