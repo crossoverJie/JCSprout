e2884f31b69540a5a1b3f0837eb9d1780ab7f715
crossoverJie <crossoverjie@gmail.com>
2018-01-17 23:41:36 +0800

    minorGC

---
+++ src/main/java/com/crossoverjie/gc/MinorGC.java
@@ -0,0 +1,27 @@
+package com.crossoverjie.gc;
+
+/**
+ * Function: Eden区不够分配时，发生minorGC
+ *
+ * @author crossoverJie
+ *         Date: 17/01/2018 22:57
+ * @since JDK 1.8
+ */
+public class MinorGC {
+
+    /**
+     * 1M
+     */
+    private static final int SIZE = 1024 * 1024 ;
+
+    public static void main(String[] args) {
+        byte[] one ;
+        byte[] four ;
+
+        one = new byte[2 * SIZE] ;
+
+
+        //再分配一个 5M 内存时，Eden区不够了，
+        four = new byte[5 * SIZE] ;
+    }
+}
