cb33aa9d36cb82fec879abbb059192de9f9d95d8
crossoverJie <crossoverjie@gmail.com>
2018-02-11 15:24:49 +0800

    :recycle: Refactoring code.

--- src/main/java/com/crossoverjie/actual/TwoThread.java
+++ src/main/java/com/crossoverjie/actual/TwoThread.java
@@ -12,27 +12,27 @@ import java.util.concurrent.locks.ReentrantLock;
  */
 public class TwoThread {
 
-    private int start = 1 ;
+    private int start = 1;
 
     /**
      * 保证内存可见性
      * 其实用锁了之后也可以保证可见性 这里用不用 volatile 都一样
      */
-    private boolean flag = false ;
+    private boolean flag = false;
 
     /**
      * 重入锁
      */
-    private final static Lock LOCK = new ReentrantLock() ;
+    private final static Lock LOCK = new ReentrantLock();
 
     public static void main(String[] args) {
-        TwoThread twoThread = new TwoThread() ;
+        TwoThread twoThread = new TwoThread();
 
         Thread t1 = new Thread(new OuNum(twoThread));
         t1.setName("t1");
 
 
-        Thread t2 = new Thread(new JiNum(twoThread)) ;
+        Thread t2 = new Thread(new JiNum(twoThread));
         t2.setName("t2");
 
         t1.start();
@@ -42,9 +42,9 @@ public class TwoThread {
     /**
      * 偶数线程
      */
-    public static class OuNum implements  Runnable{
+    public static class OuNum implements Runnable {
 
-        private TwoThread number ;
+        private TwoThread number;
 
         public OuNum(TwoThread number) {
             this.number = number;
@@ -52,26 +52,26 @@ public class TwoThread {
 
         @Override
         public void run() {
-            while (number.start <= 100){
+            while (number.start <= 100) {
 
-                    if (number.flag){
-                        try {
-                            LOCK.lock();
-                            System.out.println(Thread.currentThread().getName() + "+-+" + number.start);
-                            number.start++;
-                            number.flag = false;
+                if (number.flag) {
+                    try {
+                        LOCK.lock();
+                        System.out.println(Thread.currentThread().getName() + "+-+" + number.start);
+                        number.start++;
+                        number.flag = false;
 
 
-                        }finally {
-                            LOCK.unlock();
-                        }
-                    }else {
-                        try {
-                            Thread.sleep(10);
-                        } catch (InterruptedException e) {
-                            e.printStackTrace();
-                        }
+                    } finally {
+                        LOCK.unlock();
                     }
+                } else {
+                    try {
+                        Thread.sleep(10);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
             }
         }
     }
@@ -79,9 +79,9 @@ public class TwoThread {
     /**
      * 奇数线程
      */
-    public static class JiNum implements  Runnable{
+    public static class JiNum implements Runnable {
 
-        private TwoThread number ;
+        private TwoThread number;
 
         public JiNum(TwoThread number) {
             this.number = number;
@@ -89,26 +89,26 @@ public class TwoThread {
 
         @Override
         public void run() {
-            while (number.start <= 100){
+            while (number.start <= 100) {
 
-                    if (!number.flag){
-                        try {
+                if (!number.flag) {
+                    try {
                         LOCK.lock();
-                        System.out.println( Thread.currentThread().getName() + "+-+" + number.start);
+                        System.out.println(Thread.currentThread().getName() + "+-+" + number.start);
                         number.start++;
                         number.flag = true;
 
 
-                        }finally {
-                            LOCK.unlock();
-                        }
-                    }else {
-                        try {
-                            Thread.sleep(10);
-                        } catch (InterruptedException e) {
-                            e.printStackTrace();
-                        }
+                    } finally {
+                        LOCK.unlock();
                     }
+                } else {
+                    try {
+                        Thread.sleep(10);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
             }
         }
     }
