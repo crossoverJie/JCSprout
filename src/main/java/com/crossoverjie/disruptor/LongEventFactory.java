dd37f2f07d32abf720607aea2e43bc36be44b587
crossoverJie <crossoverjie@gmail.com>
2018-12-13 22:45:07 +0800

    :white_check_mark: Adding tests.lrumap  test

--- src/main/java/com/crossoverjie/disruptor/LongEventFactory.java
+++ src/main/java/com/crossoverjie/disruptor/LongEventFactory.java
@@ -9,11 +9,9 @@ import com.lmax.disruptor.EventFactory;
  *         Date: 2018/8/29 01:42
  * @since JDK 1.8
  */
-public class LongEventFactory implements EventFactory<LongEvent>
-{
+public class LongEventFactory implements EventFactory<LongEvent> {
     @Override
-    public LongEvent newInstance()
-    {
+    public LongEvent newInstance() {
         return new LongEvent();
     }
 }
