965067e607459c3d5d4dc5d95855cb5ba087d065
Danh Quyen <quyenpd@fimo.edu.vn>
2019-09-27 15:51:48 +0700

    adding test twosum

--- src/test/java/com/crossoverjie/algorithm/TwoSumTest.java
+++ src/test/java/com/crossoverjie/algorithm/TwoSumTest.java
@@ -6,35 +6,37 @@ import org.junit.Test;
 public class TwoSumTest {
     @Test
     public void getTwo1() throws Exception {
-        TwoSum twoSum = new TwoSum() ;
-        int[] nums ={1,3,5,7};
+        TwoSum twoSum = new TwoSum();
+        int[] nums = { 1, 3, 5, 7 };
         int[] two1 = twoSum.getTwo1(nums, 12);
         System.out.println(JSON.toJSONString(two1));
 
     }
 
     @Test
-    public void getTwo2(){
-        TwoSum twoSum = new TwoSum() ;
-        int[] nums ={1,3,5,7};
+    public void getTwo2() {
+        TwoSum twoSum = new TwoSum();
+        int[] nums = { 1, 3, 5, 7 };
         int[] two = twoSum.getTwo2(nums, 10);
         System.out.println(JSON.toJSONString(two));
 
     }
 
     @Test
-    public int[] getTwo1(int[] array, int sum) {
-        int[] result = null;
-        for (int i = 0; i < array.length; i++) {
-            int num1 = array[i];
-            for (int j = array.length - 1; j >= 0; j--) {
-                int num2 = array[j];
-                if (i != j && (num1 + num2) == sum) {
-                    result = new int[] { i, j };
-                }
-            }
-        }
-        return result;
+    public void testGetTwo1Test1() throws Exception {
+        TwoSum twoSum = new TwoSum() ;
+        int[] nums ={};
+        int[] two1 = twoSum.getTwo1(nums, 1);
+        Assert.assertEquals(two1, null); 
     }
+
+  @Test
+  public void testGetTwo1Test2() throws Exception {
+    TwoSum twoSum = new TwoSum();
+    int[] nums = {1, 2, 3, 4, 5, 6, 8, 9};
+    int[] two1 = twoSum.getTwo1(nums, 14);
+    final int[] expected = new int[]{7, 4};
+    Assert.assertArrayEquals(two1, expected);
+  }
 
 }
